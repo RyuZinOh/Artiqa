957fa61e9789a4c312f35e338ea22236bc1e98c3
RyuZinOh <140276215+RyuZinOh@users.noreply.github.com>
2025-08-26 14:39:13 +0545

    theme: bg-opacity-blur add!

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './theme/ThemeProvider.jsx'
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById('root')).render(
 
 <ThemeProvider>
   <BrowserRouter>
   <App />  
+      <ToastContainer position="top-center" autoClose={5000} />
   </BrowserRouter>
 </ThemeProvider>
 )
--- src/pages/users/Settings/ThemeSelector.jsx
+++ src/pages/users/Settings/ThemeSelector.jsx
@@ -1,27 +1,30 @@
 import { useContext, useRef, useState } from "react";
-import { PaletteIcon, CaretDownIcon, CaretUpIcon } from "@phosphor-icons/react";
+import { PaletteIcon, CaretDownIcon, CaretRightIcon, CheckIcon } from "@phosphor-icons/react";
 import { ThemeContext } from "../../../theme/Themecontext";
 import { themes } from "../../../theme";
+import { toast } from "react-toastify";
 
 export default function ThemeSelector() {
   const { theme, setTheme } = useContext(ThemeContext);
   const themeRef = useRef(null);
-  const [activePanel, setActivePanel] = useState("themes"); 
+  const [expanded, setExpanded] = useState(false); //for collapsed or expanded
+  const [activePanel, setActivePanel] = useState("themes");  //themes preset && custom
+
   const [customTheme, setCustomTheme] = useState({
     "--bgc": "",
     "--sbgc": "",
     "--color": "",
     "--border": "",
-    "--sbgurl": "none",
+    "--sbgurl": "",
     "--opacity": "",
     "--blur": ""
     });
 
-    // const [bgImage, setBgImage] = useState({
-    //     url : "",
-    //     opacity: "",
-    //     blur: ""
-    //     });
+    const [bgImage, setBgImage] = useState({
+        url : "",
+        opacity: "",
+        blur: ""
+        });
 
 
   const handleCustomThemeChange = (prop, val) =>{
@@ -40,21 +43,46 @@ export default function ThemeSelector() {
         "--opacity":  customTheme["--sbgurl"]? customTheme["--opacity"]: ""
     };
     setTheme(appliedTheme);
+    toast.success("Custom theme applied!")
   };
 
 
-//   const applyBackgroundImage = ()=>{
-//     if (bgImage.url.trim()!==""){
-//         setTheme(prev =>({
-//             ...prev,
-//         "--sbgurl": bgImage.url,
-//         "--blur": bgImage.blur,
-//         "--opacity":  bgImage.opacity
-//         }));
-//     }
-//   };
+  const applyBackgroundImage = ()=>{
+    if (bgImage.url.trim()!==""){
+        setTheme(prev =>({
+            ...prev,
+        "--sbgurl": bgImage.url,
+        "--blur": bgImage.blur,
+        "--opacity":  bgImage.opacity
+        }));
+        setCustomTheme(prev=>({
+          ...prev,
+           "--sbgurl": bgImage.url,
+        "--blur": bgImage.blur,
+        "--opacity":  bgImage.opacity
+          
+        }))
+        toast.success("Background updated!")
+    }else{
+      setTheme((prev)=>({
+        ...prev,
+          "--sbgurl": "",
+    "--opacity": "",
+    "--blur": ""
+      }));
+        setCustomTheme(prev=>({
+          ...prev,
+           "--sbgurl": bgImage.url,
+        "--blur": bgImage.blur,
+        "--opacity":  bgImage.opacity
+        }))
+        toast.info("Background updated!")
 
-//   const disableOB = bgImage.url.trim() === "";
+      
+    }
+  };
+
+  const disableOB = bgImage.url.trim() === "";
 
 
 
@@ -62,25 +90,97 @@ export default function ThemeSelector() {
   return (
         <div className="w-full px-6 mt-10">
           <div
-            onClick={() => setActivePanel(activePanel == "themes" ? null : "themes")}
-            className="flex items-center gap-2 cursor-pointer text-3xl font-bold hover:underline mt-10"
+            onClick={() => setExpanded(!expanded)} 
+             className="flex items-center gap-2 cursor-pointer text-3xl font-bold hover:underline mt-10"
           >  
             <PaletteIcon size={32} weight="bold" /> 
             <span>Themes</span>
-            {activePanel === "themes" ? 
-            <CaretUpIcon size={32} weight="bold"/> :
-             <CaretDownIcon size={32} weight="bold"/>
+            {expanded ? 
+            <CaretDownIcon size={32} weight="bold"/> :
+             <CaretRightIcon size={32} weight="bold"/>
             }
           </div>
+          {expanded && (
+            <div className="mt-4">
+              
+              {/* //bg  */}
+              <div className="rounded flex flex-col gap-3">
+                <div className="flex items-center gap-4">
+                  <label className="w-auto font-bold text-[var(--color)] text-sm">
+                    Background URL
+                  </label>
+                  <input type="text"
+                  value={bgImage.url}
+                  onChange={(e)=>
+                    setBgImage({...bgImage, url:e.target.value})
+                  }
+                  placeholder="Enter Image Url for web.."
+                  className="flex-1 px-2 py-2 text-sm rounded border-3 border-[var(--border)] text-[var(--color)]"
+                  />
+                  <button
+                  onClick={applyBackgroundImage}
+                  disabled={disableOB}
+                  className={`p-2 rounded border-3 border-[var(--border)] text-[var(--color) cursor-pointer hover:bg-[var(--sbgc)]`}
+                  >
+                    <CheckIcon size={24} weight="bold"
+                    className="text-[var(--color)]"/>
+                  </button>
+                </div>
+               
+                {!disableOB && 
+                <>
+                <div className="flex items-center gap-4">
+                  <label className="w-auto font-bold text-[var(--color)] text-sm">
+                    Opacity
+                  </label>
+                  <input type="range"
+                  min="0"
+                  max="1"
+                  step="0.05"
+                  value={bgImage.opacity}
+                  onChange={(e)=>
+                    setBgImage({
+                      ...bgImage, opacity:e.target.value
+                    })}
+                    onMouseUp={applyBackgroundImage}
+                  className="ml-15 flex-1 max-w-[150px] h-4  rounded-lg cursor-pointer"
+                  style={{
+                    accentColor: theme["--sbgc"]
+                  }}
+                  />
+                  <span className="text-sm font-bold text-[var(--color)]">{bgImage.opacity}</span>
+              </div>
+                <div className="flex items-center gap-4">
+                  <label className="w-auto font-bold text-[var(--color)] text-sm">
+                    Blur
+                  </label>
+                  <input type="range"
+                  min="0"
+                  max="1"
+                  step="0.05"
+                  value={bgImage.blur}
+                  onChange={(e)=>
+                   setBgImage({
+                      ...bgImage, blur:e.target.value
+                    })}
+                     style={{
+                    accentColor: theme["--sbgc"]
+                  }}
+                  className="ml-22 flex-1 max-w-[150px] h-4 rounded-lg cursor-pointer"
+                    onMouseUp={applyBackgroundImage}
+                  />
+                  <span className="text-sm font-bold text-[var(--color)]">{bgImage.blur}px</span>
+              </div>
+              </>
+              }
+        </div>
 
-                
-                
                 {/* toggler */}
-                <div className="flex mt-4 justify-end">
+                <div className="flex mt-4  mb-5 justify-end">
                     <button
                     onClick={() => setActivePanel(activePanel == "custom" ? "themes" : "custom")}
                     className="flex font-bold items-center gap-2 px-3 py-1.5 rounded text-sm
-                    bg-[var(--sbgc)] border-3 border-[var(--border) text-[var(--color) cursor-pointer
+                    bg-[var(--sbgc)] border-3 border-[var(--border)] text-[var(--color)] cursor-pointer
                     "
                
                >
@@ -91,7 +191,7 @@ export default function ThemeSelector() {
 
     {/* //custom  */}
  {activePanel === "custom" && (
-  <div className="mt-4 p-4 rounded-xl  bg-[var(--bgc)]">
+  <div className="mt-7 rounded-xl  bg-[var(--bgc)]">
     <div className="flex flex-col gap-3">
       {Object.entries({
         "--bgc": "Background",
@@ -143,7 +243,7 @@ export default function ThemeSelector() {
    {activePanel === "themes" && (
           <div
             ref={themeRef}
-            className="grid grid-cols-5 gap-3  items-center"
+            className="grid grid-cols-5 gap-2   items-center"
           >
             {Object.entries(themes).map(([name, colors]) => {
               const isActive = Object.entries(theme).every(
@@ -174,6 +274,7 @@ export default function ThemeSelector() {
           </div>
         )}
         </div>
-    )
-        
+        )}  
+        </div>
+    )    
 }
